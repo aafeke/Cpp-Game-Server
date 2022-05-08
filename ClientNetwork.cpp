4ca5158431b2206e138902011bba43cfdc25204a
a.faruk <->
2022-05-08 14:33:40 +0300

    client network implementation

---
+++ ClientNetwork.cpp
@@ -0,0 +1,79 @@
+#include "ClientNetwork.h"
+
+
+// Implement the declared constructor in 'ClientNetwork.h'
+ClientNetwork::ClientNetwork(void) {
+    // Create WSADATA object
+    WSADATA wsaData;
+
+    // Socket (Initially invalid)
+    ConnectSocket = INVALID_SOCKET;
+
+    // Struct to hold address info for socket to connect to
+    struct addrinfo *result = NULL, *ptr = NULL, hints;
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    if(iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        exit(1);
+    }
+
+    // Set address info
+    ZeroMemory( &hints, sizeof(hints) );
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM; // TCP
+    hints.ai_protocol = IPPROTO_TCP;
+
+    // Resolve server address and port
+    iResult = getaddrinfo("127.0.0.1", DEFAULT_PORT, &hints, &result);
+
+    if(iResult != 0) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        exit(1);
+    }
+
+    // Attempt to connect to an address until one succeeds
+    for(ptr=result; ptr != NULL; ptr=ptr->ai_next) {
+        // Define the socket
+        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+        if(ConnectSocket == INVALID_SOCKET) {
+            printf("Socket failed with error: %d\n", WSAGetLastError());
+            WSACleanup();
+            exit(1);
+        }
+        
+        // Connect to the server
+        iResult = connect(ConnectSocket, ptr->ai_addr, ptr->ai_addrlen);
+
+        if(iResult == SOCKET_ERROR) {
+            closesocket(ConnectSocket);
+            ConnectSocket = INVALID_SOCKET;
+            printf("The server is down\n");
+        }
+    }
+
+    // No longer need address info
+    freeaddrinfo(result);
+
+    // If connection failed
+    if(ConnectSocket == INVALID_SOCKET) {
+        printf("Unable to connect to the server\n");
+        WSACleanup();
+        exit(1);
+    }
+
+    // Set the socket non-blocking
+    u_long iMode = 1;
+    iResult = ioctlsocket(ConnectSocket, FIONBIO, &iMode);
+
+    if(iResult == SOCKET_ERROR) {
+        printf("ioctlsocket failed with error %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        exit(1);
+    }
+}
---
+++ ClientNetwork.h
@@ -0,0 +1,34 @@
+#ifndef CLIENT_NETWORK_H
+#define CLIENT_NETWORK_H
+
+#include <winsock2.h>
+#include <windows.h>
+#include "NetworkServices.h"
+#include <ws2tcpip.h>
+#include <stdio.h>
+
+// Size of the buffer
+#define DEFAULT_BUFLEN 512
+
+// Port to connect sockets
+#define DEFAULT_PORT "6881"
+
+// Needs to be linked with 'Ws2_32.lib', 'Mswsock.lib' and 'Advapi32.lib'
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "Advapi32.lib")
+
+class ClientNetwork {
+    public:
+        // For error checking function calls in Winsock library
+        int iResult;
+
+        // Socket for client to connect server
+        SOCKET ConnectSocket;
+
+        // Constructor / Destructor
+        ClientNetwork(void);
+        ~ClientNetwork(void);
+};
+
+#endif
