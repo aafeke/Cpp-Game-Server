cd5a1f82fe9e8b08c30aeb0961621059cb75692f
a.faruk <->
2022-05-09 13:15:05 +0300

    receive data from clients, deserialize, classify, loop in update function

--- ServerGame.cpp
+++ ServerGame.cpp
@@ -19,4 +19,42 @@ void ServerGame::update() {
 
         client_id++;
     }
+
+    receiveFromClients();
+}
+
+void ServerGame::receiveFromClients() {
+
+    Packet packet;
+
+    // Iterate through all clients
+    std::map<unsigned int, SOCKET>::iterator iter;
+
+    for(iter=network->sessions.begin(); iter != network->sessions.end(); iter++) {
+        
+        // Get the data for that client
+        int data_length = network->receiveData(iter->first, network_data);
+
+        if(data_length <= 0) {
+            // No data received
+            continue;
+        }
+
+        int i = 0;
+        while(i < (unsigned int)data_length) {
+            packet.deserialize( &(network_data[i]) );
+            i += sizeof(Packet);
+
+            switch(packet.packet_type) {
+                case INIT_CONNECTION:
+                    printf("server received init packet from client %d\n", iter->first);
+                    break;
+                case ACTION_EVENT:
+                    printf("server received action event packet from client %d\n", iter->first);
+                    break;
+                default:
+                    printf("error in packet types\n");
+            }
+        }
+    }
 }
