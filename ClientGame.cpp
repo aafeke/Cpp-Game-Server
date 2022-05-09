9f1915b575e843f6ea8b2c63f7251276a0d23739
a.faruk <->
2022-05-09 13:12:57 +0300

    send INIT_CONN packet when client initiated

--- ClientGame.cpp
+++ ClientGame.cpp
@@ -2,4 +2,15 @@
 
 ClientGame::ClientGame(void) {
     network = new ClientNetwork();
+
+    // Send init packet
+    const unsigned int packet_size = sizeof(Packet);
+    char packet_data[packet_size];
+
+    Packet packet;
+    packet.packet_type = INIT_CONNECTION;
+
+    packet.serialize(packet_data);
+
+    NetworkServices::sendMessage(network->ConnectSocket, packet_data, packet_size);
 }
