0c9d5ee0513b93a983597db28f11e5469dc7caf1
aafeke <a.farukkoca@gmail.com>
2023-02-16 09:17:09 +0300

    cross platform

--- NetworkData.h
+++ NetworkData.h
@@ -1,27 +1,6 @@
-#ifndef NETWORK_DATA_H
-#define NETWORK_DATA_H
-
-#include <string.h>
-
-#define MAX_PACKET_SIZE 1000000
-
-enum PacketTypes {
-
-    INIT_CONNECTION = 0,
-    ACTION_EVENT = 1,
-};
-
-struct Packet {
-    unsigned int packet_type;
-
-    void serialize(char * data) {
-        memcpy(data, this, sizeof(Packet));
-    }
-
-    void deserialize(char * data) {
-        memcpy(this, data, sizeof(Packet));
-    }
-};
+// This does not take care of endianness yet. Every data member
+// that is > 1 Byte in size added into the 'Packet' struct must take 
+// care of endianness during serialization and deserialization.
 
 // The packet_type field will be filled using the values
 // in the enumerator we just created.
@@ -34,5 +13,31 @@ struct Packet {
 
 // You will need to include NetworkData.h inside your 
 // ServerGame, ServerNetwork, ClientGame, and ClientNetwork classes!
+
+#ifndef NETWORK_DATA_H
+#define NETWORK_DATA_H
+
+#include <string.h>
+#include <inttypes.h>
+
+#define MAX_PACKET_SIZE 1000000
+
+enum PacketTypes {
+
+    INIT_CONNECTION = 0,
+    ACTION_EVENT = 1,
+};
+
+struct Packet {
+    uint8_t packet_type;
+
+    void serialize(char * data) {
+        memcpy(data, this, sizeof(Packet));
+    }
+
+    void deserialize(char * data) {
+        memcpy(this, data, sizeof(Packet));
+    }
+};
 
 #endif
